a55e86381aac8317fa7b1cd924d3872a7b9a9e9a
ＺＡＭＵＥＬ ＶＯＬＤＥＭＯＲＤ <79139059+ZAMUEL-MX@users.noreply.github.com>
2021-02-18 23:39:36 +0700

    Update Acmi.py

--- Acmi.py
+++ Acmi.py
@@ -1,4 +1,4 @@
-Hayoo lo aj9 mau ngapain semvak!!
+#encrypt by zamuel -> Hayoo lo aj9 -> mau ngapain semvak!!
 
 import marshal,zlib,base64
 exec(marshal.loads(zlib.decompress(base64.b64decode("eJzdPFtsG9l1dyiREilRT9uy1l575Kf8EJ8SJdmr9dKP9Sq2ZYey12s5BjPijKQRySE9M7SljRSk8SZ95LG76Wa3bfaRBNt8BEWKpAG6QZMGBfrRj3wUaFG0KFAUm88GyH70o0BbpOecO3fIoUhKazgIGj0u79zHmXvPPa977rnMMefHD//Pwb/1Mx9jKvxJrMDYgpuX2IIk8j624BP5NrbQJvLtbKFd5P1swS/yAbYQEPkOttAh8p1soVPkg2whKPIhthAS+S62XuWczkkrwFtttF98f66zzDdJ+/zhejmeBgof6At6A92BkPBwYAvGAhILX/bgl3BgeBkcHdwFP6PBPsh/RXT8Kb6/wEY0ZFH"))))
